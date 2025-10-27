c2a7f5ff7aa25ba6da377ce924d444e72227a969
Kate Korsaro <kate.korsaro@gmail.com>
2025-10-27 08:41:54 +0100

    review: rewrite roller.ydx in a more sensible way

--- 01.workspace/rusty_flagon_lib/src/imp/roller_ydx.rs
+++ 01.workspace/rusty_flagon_lib/src/imp/roller_ydx.rs
@@ -1,11 +1,10 @@
 use crate::*;
 
 impl Roller {
-    pub(crate) fn ydx(&mut self, mut dice: u8, sides: u8) -> u16 {
+    pub(crate) fn ydx(&mut self, dice: u8, sides: u8) -> u16 {
         let mut result = 0u16;
-        while dice > 0 {
+        for _ in 1..=dice {
             result += self.dx(sides) as u16;
-            dice -= 1;
         }
         result
     }
